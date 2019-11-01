26b2ce6c1a59761e7af3a270c6602ee2f9b7f81b
Matthew Kirk Jensen <matthew.k.jensen@gmail.com>
2019-11-01 13:55:17 -0400

    added return for review function

--- jobs/app.py
+++ jobs/app.py
@@ -73,4 +73,4 @@ def review(employer_id):
         execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',
                     (review, rating, title, date, status, employer_id), commit=True)
         return redirect(url_for('employer', employer_id=employer_id))
-    render_template('review.html', employer_id=employer_id)
+    return render_template('review.html', employer_id=employer_id)
